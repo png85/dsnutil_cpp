af7a562fe58ba350e678d0df8bbafff245ecbdc9
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-04-17 17:54:23 +0200

    Merge dsn::log::stream_interceptor template
    This commit adds a new class template to the logging API which can be used
    to intercept any std::ostream and route its output through a boost severity
    logger.
    
    The template works similar to the old intercept_stream code in libdsnutil
    but is more flexible.
    
    Signed-off-by: Peter Hille (png!das-system) <peter@das-system-networks.de>

---
+++ examples/boost_log-stream_interceptor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <boost/log/sources/severity_feature.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/trivial.hpp>
+
+#include <dsnutil/log/init.h>
+#include <dsnutil/log/stream_interceptor.h>
+
+int main() {
+    dsn::log::init();
+
+    boost::log::sources::severity_logger<boost::log::trivial::severity_level> log;
+
+    // intercept std::cout into log
+    dsn::log::stream_interceptor<> cout_interceptor(std::cout, log, "std::cout", boost::log::trivial::info);
+    std::cout << "Hello to stdout..." << std::endl;
+
+    // intercept std::cerr using separate logger
+    dsn::log::stream_interceptor<> cerr_interceptor(std::cerr, "std::cerr", boost::log::trivial::debug);
+    std::cerr << "Hello to cerr (with local logger!)..." << std::endl;
+
+    return EXIT_SUCCESS;
+}
