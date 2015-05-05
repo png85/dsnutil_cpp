cde052f4c710fd87e7958e408b5029616da7605a
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-05-05 20:40:19 +0200

    Clean up dsn::log::init()

--- src/log/init.cpp
+++ src/log/init.cpp
@@ -21,26 +21,32 @@ void dsn::log::init(const std::string& filename)
 
     dsn::log::SinkManager& manager = dsn::log::SinkManager::getInstance();
 
-    // create backend for std::clog sink
-    boost::shared_ptr<boost::log::sinks::text_ostream_backend> backend = boost::make_shared<boost::log::sinks::text_ostream_backend>();
+    // create backend and sink for std::clog
+    typedef boost::log::sinks::text_ostream_backend backend_t;
+    boost::shared_ptr<backend_t> backend = boost::make_shared<backend_t>();
     backend->add_stream(boost::shared_ptr<std::ostream>(&std::clog, dsn::null_deleter()));
     backend->auto_flush(true);
 
-    // create the actual sink
-    typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend> sink_t;
+    typedef boost::log::sinks::synchronous_sink<backend_t> sink_t;
     boost::shared_ptr<sink_t> sink(new sink_t(backend));
     sink->set_formatter(boost::log::parse_formatter(format));
 
-    // register sink with manager
     manager.add("std::clog", sink);
-    BOOST_LOG_TRIVIAL(trace) << "Initialized default console log settings";
+    BOOST_LOG_TRIVIAL(trace) << "Initialized default console log settings (std::clog)";
 
     if (filename.length() > 0) {
-        // setup default logfile with given name/pattern, rotation at 1GB size or daily at 00:00:00
-        boost::log::add_file_log(boost::log::keywords::file_name = filename,
-                                 boost::log::keywords::format = format,
-                                 boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
-                                 boost::log::keywords::rotation_size = 1024 * 1024 * 1024);
+        // setup backend and sink for the given logfile name, default rotation parameters are:
+        //
+        // - daily at 00:00:00
+        // - at 1GB size
+        typedef boost::log::sinks::text_file_backend backend_t;
+        boost::shared_ptr<backend_t> fileBackend = boost::make_shared<backend_t>(boost::log::keywords::file_name = filename,
+                                                                                 boost::log::keywords::rotation_size = 1024 * 1024 * 1024,
+                                                                                 boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
+                                                                                 boost::log::keywords::format = format);
+        typedef boost::log::sinks::synchronous_sink<backend_t> sink_t;
+        boost::shared_ptr<sink_t> sink(new sink_t(fileBackend));
+        manager.add("file://" + filename, sink);
         BOOST_LOG_TRIVIAL(trace) << "Initialized default settings for file: " << filename;
     }
 }
