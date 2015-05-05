3ef8019e69bb4a3e6400c76c492c71c6fcc20aaa
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-05-05 19:18:48 +0200

    Add class: dsn::log::SinkManager

---
+++ include/dsnutil/log/sinkmanager.h
@@ -0,0 +1,35 @@
+#ifndef SINKMANAGER_H
+#define SINKMANAGER_H
+
+#include <dsnutil/singleton.h>
+#include <dsnutil/log/base.h>
+
+#include <map>
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include <boost/log/sinks.hpp>
+
+namespace dsn {
+namespace log {
+
+    class SinkManager : public dsn::Singleton<SinkManager>, public Base<SinkManager> {
+        friend class dsn::Singleton<SinkManager>;
+        SinkManager();
+        ~SinkManager();
+
+    public:
+        typedef boost::shared_ptr<boost::log::sinks::sink> sink_ptr;
+
+        bool exists(const std::string& name) const;
+        bool add(const std::string& name, const sink_ptr& sink);
+        bool remove(const std::string& name);
+
+        std::vector<std::string> sinks() const;
+
+    private:
+        std::map<std::string, sink_ptr> m_sinks;
+    };
+}
+}
+
+#endif // SINKMANAGER_H
---
+++ src/log/sinkmanager.cpp
@@ -0,0 +1,58 @@
+#include <dsnutil/log/sinkmanager.h>
+#include <dsnutil/map_sort.h>
+
+using namespace dsn::log;
+
+SinkManager::SinkManager()
+{
+    BOOST_LOG_SEV(log, severity::trace) << "Sink manager initialized.";
+}
+
+SinkManager::~SinkManager()
+{
+    BOOST_LOG_SEV(log, severity::trace) << "Sink manager shutting down.";
+    for (auto& sink : m_sinks) {
+        BOOST_LOG_SEV(log, severity::trace) << "Removing leftover sink: " << sink.first;
+        boost::log::core::get()->remove_sink(sink.second);
+    }
+    m_sinks.clear();
+}
+
+bool SinkManager::exists(const std::string& name) const
+{
+    return (m_sinks.find(name) != m_sinks.end());
+}
+
+bool SinkManager::add(const std::string& name, const SinkManager::sink_ptr& sink)
+{
+    if (exists(name)) {
+        BOOST_LOG_SEV(log, severity::debug) << "Overwriting '" << name << "' sink...";
+        if (!remove(name)) {
+            BOOST_LOG_SEV(log, severity::error) << "Failed to remove existing '" << name << "' sink!";
+            return false;
+        }
+    }
+
+    boost::log::core::get()->add_sink(sink);
+    m_sinks[name] = sink;
+    BOOST_LOG_SEV(log, severity::debug) << "Added sink: " << name;
+    return true;
+}
+
+bool SinkManager::remove(const std::string& name)
+{
+    if (!exists(name)) {
+        BOOST_LOG_SEV(log, severity::warning) << "Tried to remove non-existant sink: " << name;
+        return false;
+    }
+
+    boost::log::core::get()->remove_sink(m_sinks[name]);
+    m_sinks.erase(name);
+    BOOST_LOG_SEV(log, severity::debug) << "Removed sink: " << name;
+    return true;
+}
+
+std::vector<std::string> SinkManager::sinks() const
+{
+    return dsn::map_keys(m_sinks);
+}
