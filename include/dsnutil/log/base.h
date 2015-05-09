528517603a24ebdd43dc1928f127984025bed425
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-05-09 13:51:05 +0200

    reformat Doxygen comments

--- include/dsnutil/log/base.h
+++ include/dsnutil/log/base.h
@@ -21,23 +21,23 @@ namespace dsn {
 namespace log {
 
     /** \brief Base template for classes using boost::log based logging
- *
- * This template implements a basic logging API using boost::log which can be added to any class
- * by deriving from the template instanced for the class itself.
- *
- * The class will then inherit a member named \a log which can be used to log messages using the
- * BOOST_LOG_SEV macro and the severity levels defined in boost::log::trivial::severity_level.
- *
- * Log messages will have a string representation of the mangled class name and the value of the
- * 'this' pointer added as %Class% and %This% so it's possible to tag log messages with precise
- * debug information.
- *
- * \param T Derived class for which the logging API shall be generated
- *
- * \version 1.0 Initial implementation
- *
- * \author Peter Hille (png!das-system) <peter@das-system-networks.de>
- */
+     *
+     * This template implements a basic logging API using boost::log which can be added to any class
+     * by deriving from the template instanced for the class itself.
+     *
+     * The class will then inherit a member named \a log which can be used to log messages using the
+     * BOOST_LOG_SEV macro and the severity levels defined in boost::log::trivial::severity_level.
+     *
+     * Log messages will have a string representation of the mangled class name and the value of the
+     * 'this' pointer added as %Class% and %This% so it's possible to tag log messages with precise
+     * debug information.
+     *
+     * \param T Derived class for which the logging API shall be generated
+     *
+     * \version 1.0 Initial implementation
+     *
+     * \author Peter Hille (png!das-system) <peter@das-system-networks.de>
+     */
     template <typename T>
     class Base {
     public:
@@ -54,10 +54,10 @@ namespace log {
 
     protected:
         /** \brief Severity logger for all messages from derived class
-     *
-     * This logger can be used via BOOST_LOG_SEV(log, <severity_level>) << message to log diagnostic
-     * information from the instance of the derived class.
-     */
+         *
+         * This logger can be used via BOOST_LOG_SEV(log, <severity_level>) << message to log diagnostic
+         * information from the instance of the derived class.
+         */
         boost::log::sources::severity_logger<severity> log;
     };
 }
