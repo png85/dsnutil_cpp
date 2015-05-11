fbb4f0f1bea9e41d6052f57fde0bf1e9903b9a59
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-05-12 00:28:34 +0200

    Remove CheckCXX11Features.cmake since CMake checks for C++11 features itself now.

--- cmake-modules/c++11-test-initializer_list.cpp
+++
@@ -1,9 +0,0 @@
-#include <list>
-#include <string>
-
-using namespace std;
-
-int main() {
-  list<int> l{1, 2, 3};
-  return 0;
-}
--- cmake-modules/c++11-test-std_array.cpp
+++
@@ -1,6 +0,0 @@
-#include <array>
-
-int main() {
-  std::array<int, 3> a;
-  return 0;
-}
